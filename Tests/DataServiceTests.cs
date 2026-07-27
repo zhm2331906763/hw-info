afd674ccbb2535cbb4ede5504667aae582d0fa9a
zhm2331906763 <2331906763@qq.com>
2026-07-27 09:46:28 +0800

    Task 1: upgrade to .NET 4.8, add SQLite + xUnit packages

---
+++ Tests/DataServiceTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace HW_info.Tests
+{
+    public class DataServiceTests
+    {
+        [Fact]
+        public void Placeholder()
+        {
+            Assert.True(true);
+        }
+    }
+}
